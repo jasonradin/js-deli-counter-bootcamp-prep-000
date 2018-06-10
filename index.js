9547c2c9dec41a8eaf278c02081df363d53fd1e4
jason.radin@gmail.com <jason.radin@gmail.com>
2018-06-10 17:09:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function nowServing() {
   if (katzDeliLine.length === 0) {
     return "There is nobody waiting to be served!"
   }
-  else return katzDeliLine[0]
+  else return "Currently serving " + katzDeliLine[0] + "."
   katzDeliLine.shift()
 }
