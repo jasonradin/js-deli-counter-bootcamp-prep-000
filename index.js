050c93b923851e494fe913e568779fb03efdf3bc
jason.radin@gmail.com <jason.radin@gmail.com>
2018-06-10 16:48:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var katzDeli = [];
 
-function takeANumber(katzDeliLine, new) {
+function takeANumber(katzDeliLine, new ) {
   katzDeli.push(katzDeliLine, new); 
   var position = katzDeli.length; 
   return position; 
