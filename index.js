2c495651e48b4f7ef347f7a00ac1be4ce39293f8
jason.radin@gmail.com <jason.radin@gmail.com>
2018-06-10 16:52:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ var katzDeli = [];
 function takeANumber(katzDeliLine, name) {
   katzDeli.push(katzDeliLine, name); 
   var position = katzDeli.length; 
-  return position; 
+  return "Welcome, " + name + ". You are number " position + "in line."
 }
