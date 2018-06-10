76bcffe6c50db4feb2cc20ca931d1ddf76e5538a
jason.radin@gmail.com <jason.radin@gmail.com>
2018-06-10 17:50:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,6 +16,10 @@ function nowServing(katzDeliLine) {
 }
 
 function currentLine(katzDeliLine) {
+  if(katzDeliLine.length === 0) {
+    return "The line is currently empty."
+  }
+  else 
   var line = []; 
   for (var i = 0; i < katzDeliLine.length; i +=1) {
     line.push([i+1] + ". " + katzDeliLine[i])
