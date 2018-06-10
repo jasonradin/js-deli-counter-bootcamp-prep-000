08048ceff71d061e03435ed21dbde050c79db969
jason.radin@gmail.com <jason.radin@gmail.com>
2018-06-10 17:48:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,4 +20,5 @@ function currentLine(katzDeliLine) {
   for (var i = 0; i < katzDeliLine.length; i +=1) {
     line.push(" " + [i+1] + ". " + katzDeliLine[i])
   }
+  return "The line is currently " + line; 
 }
